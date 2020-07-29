3a5b4b5711ffd81b047902947b8a46fb666f8c41
arorasoham9 <68672198+arorasoham9@users.noreply.github.com>
2020-07-29 13:49:51 +0530

    Add files via upload

--- src/comment.java
+++ src/comment.java
@@ -2,37 +2,40 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.lang.*;
 import java.lang.StringBuilder;
+
 public class comment {
 
     private String comtext;
-    private String commenterID;
+    //private String commenterID;
     private String commentername;
-    private String commentID;
+    //private String commentID;
     private int likes;
-    private String postID;
+    //private String postID;
     private String time;
     //private int
+    private static final int SIZE = 15;
 
-    public comment(){
+
+    public comment() {
     }
 
-    public comment(String comtext, String commenterID, String commentID, int likes, String postID,String commentername) {
+    public comment(String commentername, String comtext, int likes, String Time ) {
         this.comtext = comtext;
-        this.commenterID = commenterID;
-        this.commentID = commentID;
+        //this.commenterID = commenterID;
+        //this.commentID = commentID;
         this.likes = likes;
-        this.postID = postID;
-        this.commentername= commentername;
+        //this.postID = postID;
+        this.commentername = commentername;
     }
 
-    public String getPostID() {
-        return postID;
-    }
+//    public String getPostID() {
+//        return postID;
+//    }
 
 
-    public void setPostID(String postID) {
-        this.postID = postID;
-    }
+//    public void setPostID(String postID) {
+//        this.postID = postID;
+//    }
 
 
 //    public comment(String comtext, String commenterID, int likes){
@@ -42,36 +45,36 @@ public class comment {
 //    }
 
 
-
-    public String getComtext(){
+    public String getComtext() {
         return comtext;
     }
 
-    public String getCommenterID(){
-        return commenterID;
-    }
+//    public String getCommenterID(){
+//        return commenterID;
+//    }
 
     public int getLikes() {
         return likes;
     }
+
     public void setLikes(int likes) {
         this.likes = likes;
     }
 
-    public void setComtext(String comtext){
-        this.comtext= comtext;
+    public void setComtext(String comtext) {
+        this.comtext = comtext;
     }
-    public void setCommenterID(String commenterID){
-        this.commenterID= commenterID;
-    }
+//    public void setCommenterID(String commenterID){
+//        this.commenterID= commenterID;
+//    }
 
-    public String getCommentID() {
-        return commentID;
-    }
+//    public String getCommentID() {
+//        return commentID;
+//    }
 
-    public void SetTime(){
-        Date date=java.util.Calendar.getInstance().getTime();
-         time= String.format("%s",date);
+    public void SetTime() {
+        Date date = java.util.Calendar.getInstance().getTime();
+        time = String.format("%s", date);
     }
 
     public String getCommentername() {
@@ -86,10 +89,30 @@ public class comment {
         return time;
     }
 
-    public String toString(){
-        StringBuilder sb= new StringBuilder();
-        sb.append("\t"+getCommentername()+" @"+getCommenterID()+" ."+getTime()+"\n");
-        sb.append("\t"+getComtext()+"\n");
-        return sb.toString();
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t" + getCommentername() + " @ " + getTime() + "\n");
+        String format = "";
+        //sb.append("\t"+getComtext()+"\n");
+        int length = getComtext().length();
+        int j = 0;
+
+        for (int i = 0; i < length; i++) {
+            format += getComtext().charAt(i);
+
+            if (j > SIZE) {
+                if (getComtext().charAt(i) == '.' || getComtext().charAt(i) == ' ') {
+                    format += "\n";
+                    while (i < length - 1 && getComtext().charAt(++i) == ' ') ;
+                    i--;
+                    j = 0;
+                }
+            }
+            j++;
+        }
+
+        return format;
     }
+    //return sb.toString();
 }
+
