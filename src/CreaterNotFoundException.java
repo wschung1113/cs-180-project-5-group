3dd73297f0bfcec3dd4765114f924b2b3ac84922
arorasoham9 <68672198+arorasoham9@users.noreply.github.com>
2020-07-30 20:06:04 +0530

    Add files via upload

---
+++ src/CreaterNotFoundException.java
@@ -0,0 +1,13 @@
+public class CreaterNotFoundException extends Exception {
+        /**
+         * Constructs a new BookNotFoundException with a specified
+         * message
+         *
+         * @param message the message provided. The message is saved for later retrieval by
+         *                the getMessage() method.
+         */
+        public CreaterNotFoundException(String message) {
+            super(message);
+        }
+    }
+
