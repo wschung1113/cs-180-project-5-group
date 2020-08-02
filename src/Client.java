f0954adc22f5a935dc75eb80fe73914c051fa94a
leodao07 <58986403+leodao07@users.noreply.github.com>
2020-08-03 05:45:32 +0700

    I change to write and read Object between server and client.
    I still have some problem of returning the boolean value

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -17,31 +14,25 @@ public class Client {
 
     public boolean connect() throws IOException, ClassNotFoundException {
         Socket socket = new Socket("localhost", 4242);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.flush();
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 
-        writer.write(message);
-        writer.println();
-        writer.flush(); // ensure data is sent to the server
+
+
+        // Send message to the server
+        oos.writeObject(message);
+        oos.flush(); // ensure data is sent to the server
 
         //Receive
-        int c;
-        StringBuilder response = new StringBuilder();
-        while ((c = reader.read()) != -1) {
-            response.append((char) c);
-        }
+        // I don't know how to return the suitable value based on whether the user is trying to login or register. Any idea?
+        boolean loginValid = (boolean) ois.readObject();
+        boolean registerValid = (boolean) ois.readObject();
 
-//        System.out.println(response);
+        //Close necessary objects
+        oos.close();
+        ois.close();
 
-        if (response.toString().contains("Correct") || response.toString().contains("Registered")) {
-            return true;
-        }
-        if (response.toString().contains("Incorrect") || response.toString().contains("Existed")) {
-            return false;
-        }
-
-        writer.close();
-        reader.close();
         return false;
     }
 }
