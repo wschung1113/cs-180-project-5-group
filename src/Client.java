4da9278898734a961a988eb597ac5d7e615d654e
lingbofang <fang269@purdue.edu>
2020-08-02 23:26:32 -0400

    Revert "Change connect() to main and call LoginGUI"
    This reverts commit efbabc71293df7ef2a3ff119fb7837026dbc2775.

--- src/Client.java
+++ src/Client.java
@@ -6,25 +6,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
+    String message;
 
-    public Client() {
-
+    public Client(String message) {
+        this.message = message;
     }
 
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public boolean connect() throws IOException, ClassNotFoundException {
         Socket socket = new Socket("localhost", 4242);
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.flush();
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        LoginGUI loginGUI = new LoginGUI();
+
+
+
+        // Send message to the server
+        oos.writeObject(message);
+        oos.flush(); // ensure data is sent to the server
+
+        //Receive
+        // I don't know how to return the suitable value based on whether the user is trying to login or register. Any idea?
+        boolean loginValid = (boolean) ois.readObject();
+        boolean registerValid = (boolean) ois.readObject();
 
         //Close necessary objects
         oos.close();
         ois.close();
 
-        loginGUI.run();
-
-
+        return false;
     }
 }
