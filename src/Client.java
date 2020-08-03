efbabc71293df7ef2a3ff119fb7837026dbc2775
leodao07 <58986403+leodao07@users.noreply.github.com>
2020-08-03 09:27:38 +0700

    Change connect() to main and call LoginGUI

--- src/Client.java
+++ src/Client.java
@@ -6,33 +6,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client {
-    String message;
 
-    public Client(String message) {
-        this.message = message;
+    public Client() {
+
     }
 
-    public boolean connect() throws IOException, ClassNotFoundException {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Socket socket = new Socket("localhost", 4242);
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.flush();
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-
-
-
-        // Send message to the server
-        oos.writeObject(message);
-        oos.flush(); // ensure data is sent to the server
-
-        //Receive
-        // I don't know how to return the suitable value based on whether the user is trying to login or register. Any idea?
-        boolean loginValid = (boolean) ois.readObject();
-        boolean registerValid = (boolean) ois.readObject();
+        LoginGUI loginGUI = new LoginGUI();
 
         //Close necessary objects
         oos.close();
         ois.close();
 
-        return false;
+        loginGUI.run();
+
+
     }
 }
